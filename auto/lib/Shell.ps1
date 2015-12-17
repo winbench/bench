035cf559932f8ce70232076efe142f723aaab5a5
Tobias Kiertscher <tobias.kiertscher@bosch.com>
2015-12-17 12:44:03 +0100

    show project info

--- auto/lib/Shell.ps1
+++ auto/lib/Shell.ps1
@@ -25,4 +25,39 @@ if ($message) {
     Write-Output ""
 }
 
+$props = @()
+if (test-Path ".svn" -PathType Container) {
+    $props += "under SVN version control"
+}
+if (Test-Path ".git" -Pathtype Container) {
+    $props += "under Git version control"
+}
+if (Test-Path "bower.json" -PathType Leaf) {
+    if (Test-Path "bower_components" -PathType Container) {
+        $props += "consuming Bower components"
+    } else {
+        $props += "consuming Bower components, run 'bower install' to load them"
+    }
+}
+if (Test-Path "package.json" -PathType Leaf) {
+    if (Test-Path "node_modules" -PathType Container) {
+        $props += "a Node Package"
+    } else {
+        $props += "a Node Package, run 'npm install' to load dependencies"
+    }
+}
+if (Test-Path "gulpfile.js" -PathType Leaf) {
+    $props += "automated with Gulp"
+}
+if ((Test-Path "Gruntfile.js" -PathType Leaf) -or (Test-Path "Gruntfile.coffee" -PathType Leaf)) {
+    $props += "automated with Grunt"
+}
+if ($props.Count -gt 0) {
+    Write-Output "The Project is"
+    foreach ($p in $props) {
+        Write-Output "- $p"
+    }
+    Write-Output ""
+}
+
 powershell -NoLogo -NoProfile -NoExit
