0953869a5e66a23e86c22efb3a68f5ead7d0a4e4
Tobias Kiertscher <dev@mastersign.de>
2015-12-29 18:12:37 +0100

    added automatic configuration for GIT user and SSH key pair

--- auto/apps/git.setup.ps1
+++ auto/apps/git.setup.ps1
@@ -1,6 +1,25 @@
 $git = App-Exe Git
 if (!$git) { throw "Git not found" }
-& $git config --global "push.default" "simple"
+
+$pushDefault = & $git config --global push.default
+$user = & $git config --global user.name
+$email = & $git config --global user.email
+
+if (!$pushDefault) {
+    & $git config --global "push.default" "simple"
+}
+if (!$user -or !$email) {
+    Write-Host "Configure your GIT identity"
+    if (!$user) {
+        $user = Read-Host "User Name"
+        & $git config --global "user.name" $user
+    }
+    if (!$email) {
+        $email = Read-Host "Email"
+        & $git config --global "user.email" $email
+    }
+}
+
 if (Get-ConfigValue UseProxy) {
     & $git config --global "http.proxy" $(Get-ConfigValue HttpProxy)
     & $git config --global "https.proxy" $(Get-ConfigValue HttpsProxy)
@@ -9,4 +28,12 @@ if (Get-ConfigValue UseProxy) {
     & $git config --global --unset "http.proxy"
     & $git config --global --unset "https.proxy"
     & $git config --global --unset "url.https://.insteadof"
+}
+
+if (!(Test-Path "$env:USERPROFILE\.ssh")) {
+    $gitDir = App-Dir Git
+    $keygen = "$gitDir\usr\bin\ssh-keygen.exe"
+    $_ = mkdir "$env:USERPROFILE\.ssh"
+    & $keygen -t rsa -b 4096 -f "$env:USERPROFILE\.ssh\id_rsa" -C "$user <$email>"
+    notepad "$env:USERPROFILE\.ssh\id_rsa.pub"
 }
