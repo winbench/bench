20ed3a4a34ffefe4b2e2f5b692211514320dda18
Tobias Kiertscher <dev@mastersign.de>
2016-01-02 05:41:18 +0100

    added missing debug handling

--- auto/lib/Prepare-Config.ps1
+++ auto/lib/Prepare-Config.ps1
@@ -3,6 +3,8 @@ param ([switch]$debug)
 $scriptsLib = [IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
 . "$scriptsLib\bench.lib.ps1"
 
+Set-Debugging $debug
+
 $customAppIndex = Get-ConfigPathValue CustomAppIndex
 if (!(Test-Path $customAppIndex)) {
     $customAppIndexTemplate = Get-ConfigPathValue CustomAppIndexTemplate
