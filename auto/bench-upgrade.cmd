c82e5193abfdf5e89e2ebaa6f3f292f2c5be187e
Tobias Kiertscher <dev@mastersign.de>
2016-01-02 05:41:47 +0100

    added GIT pull to bench-upgrade

--- auto/bench-upgrade.cmd
+++ auto/bench-upgrade.cmd
@@ -2,6 +2,7 @@
 CALL "%~dp0\init.cmd"
 CALL runps Clear-Apps
 CALL runps Clear-Downloads
+CALL runps Update-BenchRepo
 CALL runps Download-Apps
 CALL runps Setup-Apps
 CALL "%~dp0\env.cmd"
---
+++ auto/lib/Update-BenchRepo.ps1
@@ -0,0 +1,23 @@
+param ([switch]$debug)
+
+$scriptsLib = [IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
+. "$scriptsLib\bench.lib.ps1"
+. "$scriptsLib\appconfig.lib.ps1"
+
+Set-Debugging $debug
+
+cd $Script:rootDir
+
+$git = App-Exe Git
+if ($git) {
+    Debug "Found GIT executable: $git"
+    if (Test-Path ".git") {
+        Debug "Pulling from Bench repository"
+        & $git reset --hard
+        & $git pull
+    } else {
+        Debug "Bench root directory is not a GIT working copy"
+    }
+} else {
+    Write-Warning "GIT executable not found"
+}
