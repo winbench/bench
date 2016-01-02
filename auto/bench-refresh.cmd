9cb58706e75d35a060573e0c15f295c1e7af0da5
Tobias Kiertscher <dev@mastersign.de>
2016-01-02 13:40:43 +0100

    added pause commands into bench-refresh and bench-upgrade

--- auto/bench-refresh.cmd
+++ auto/bench-refresh.cmd
@@ -5,4 +5,5 @@ CALL runps Download-Apps
 CALL runps Setup-Apps
 CALL "%~dp0\env.cmd"
 CD "%~dp0\.."
+PAUSE
 runps Shell
--- auto/bench-upgrade.cmd
+++ auto/bench-upgrade.cmd
@@ -7,4 +7,5 @@ CALL runps Download-Apps
 CALL runps Setup-Apps
 CALL "%~dp0\env.cmd"
 CD "%~dp0\.."
+PAUSE
 runps Shell
