590cb59317c05a6f916d964c9fe81ee674468553
Tobias Kiertscher <dev@mastersign.de>
2016-01-06 19:30:06 +0100

    pass arguments to management scripts

--- auto/bench-download.cmd
+++ auto/bench-download.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
 CALL "%~dp0\init.cmd"
 CALL runps Prepare-Config
-CALL runps Download-Apps
+CALL runps Download-Apps %*
 CD "%~dp0\.."
 PAUSE
 
--- auto/bench-setup.cmd
+++ auto/bench-setup.cmd
@@ -1,8 +1,8 @@
 @ECHO OFF
 CALL "%~dp0\init.cmd"
 CALL runps Prepare-Config
-CALL runps Download-Apps
-CALL runps Setup-Apps
+CALL runps Download-Apps %*
+CALL runps Setup-Apps %*
 CALL runps Finalize-Setup
 CALL "%~dp0\env.cmd"
 CD "%~dp0\.."
