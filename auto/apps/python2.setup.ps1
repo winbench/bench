beb7dd8d8bb6063109a180c7fd59b40eb87c91e5
Tobias Kiertscher <dev@mastersign.de>
2016-01-20 16:48:00 +0100

    improving Python support with version-numbered wrapper 'python2.cmd' and 'python3.cmd' skipping call to ensurepip if pip already exists

--- auto/apps/python2.setup.ps1
+++ auto/apps/python2.setup.ps1
@@ -3,4 +3,14 @@ if (!$python) { throw "Python2 not found" }
 
 $pythonDir = App-Dir Python2
 
-& $python -m ensurepip
+$pythonWrapper = [IO.Path]::Combine($pythonDir, "python2.cmd")
+if (!(Test-Path $pythonWrapper)) {
+    Write-Host "Creating wrapper to call Python 2 via 'python2' ..."
+    "@CALL `"%~dp0\python.exe`" %*" | Out-File $pythonWrapper -Encoding default
+}
+
+$pipPackageDir = [IO.Path]::Combine($pythonDir, "lib", "site-packages", "pip")
+if (!(Test-Path $pipPackageDir -PathType Container)) {
+    Write-Host "Setting up PIP ..."
+    & $python -m ensurepip
+}
