1b95d5138d76d396022e59a83ac2d80d5bca56e0
Tobias Kiertscher <tobias.kiertscher@bosch.com>
2016-01-27 10:34:18 +0100

    improved Expand-Value by moving boolean detection to the beginning

--- auto/lib/config.lib.ps1
+++ auto/lib/config.lib.ps1
@@ -57,15 +57,15 @@ function Expand-Placeholder([string]$placeholder) {
 
 function Expand-Value($value) {
     if ($value -is [string]) {
-        $value = $valueExpandPattern.Replace($value, [Text.RegularExpressions.MatchEvaluator]{
-            param ($m)
-            return Expand-Placeholder $m.Groups["var"].Value
-        })
         if ($value -ieq "true") {
             return $true
         } elseif ($value -ieq "false") {
             return $false
         }
+		$value = $Script:valueExpandPattern.Replace($value, [Text.RegularExpressions.MatchEvaluator]{
+            param ($m)
+            return Expand-Placeholder $m.Groups["var"].Value
+        })
     }
     return $value
 }
