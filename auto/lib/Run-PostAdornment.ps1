4d2df683f55e395a44d00cce2bfff75dc2009b6c
Tobias Kiertscher <tobias.kiertscher@bosch.com>
2016-02-10 15:28:02 +0100

    change window title for pre-run and post-run scripts

--- auto/lib/Run-PostAdornment.ps1
+++ auto/lib/Run-PostAdornment.ps1
@@ -2,13 +2,13 @@ param (
     $name,
     $executable
 )
+$Host.UI.RawUI.WindowTitle = "Bench - $name - Post-Run"
 
 $Script:scriptsLib = [IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
 . "$Script:scriptsLib\bench.lib.ps1"
 
 $customPostFile = "$Script:scriptsLib\..\apps\$($name.ToLowerInvariant()).post-run.ps1"
 if (Test-Path $customPostFile) {
-    Write-Host "Post-Execution Adornment for $name ..."
     $old = Set-StopOnError $false
     . $customPostFile
     $_ = Set-StopOnError $old
--- auto/lib/Run-PreAdornment.ps1
+++ auto/lib/Run-PreAdornment.ps1
@@ -2,14 +2,16 @@ param (
     $name,
     $executable
 )
+$Host.UI.RawUI.WindowTitle = "Bench - $name - Pre-Run"
 
 $Script:scriptsLib = [IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
 . "$Script:scriptsLib\bench.lib.ps1"
 
 $customPreFile = "$Script:scriptsLib\..\apps\$($name.ToLowerInvariant()).pre-run.ps1"
 if (Test-Path $customPreFile) {
-    Write-Host "Pre-Execution Adornment for $name ..."
     $old = Set-StopOnError $false
     . $customPreFile
     $_ = Set-StopOnError $old
 }
+
+$Host.UI.RawUI.WindowTitle = "Bench - $name"
