1228b9959c4b4244acd0aca16e7ecb4f4555be34
Tobias Kiertscher <dev@mastersign.de>
2016-04-23 12:51:43 +0200

    added reg.lib.ps1 to custom script environment (postgres.setup.ps1 installs default registry isolation file)

--- auto/lib/Run-CustomScript.ps1
+++ auto/lib/Run-CustomScript.ps1
@@ -5,6 +5,7 @@ param (
 
 $scriptsLib = [IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
 . "$scriptsLib\bench.lib.ps1"
+. "$scriptsLib\reg.lib.ps1"
 
 Write-Host "Running custom script $([IO.Path]::GetFileName($scriptPath))..."
 
