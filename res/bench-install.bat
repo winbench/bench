de8702b425a271a2317fd7c67f018a57ae59dc0e
Tobias Kiertscher <dev@mastersign.de>
2016-04-30 15:52:11 +0200

    updated bench-install

--- res/bench-install.bat
+++ res/bench-install.bat
@@ -1,30 +1,58 @@
-@ECHO OFF
+REM @ECHO OFF
 SET ROOT=%~dp0
-SET ZIPURL=https://github.com/mastersign/bench/archive/master.zip
-SET ZIPFILE=%ROOT%bench.zip
 
-ECHO.Download ZIP archive ...
-CD "%ROOT%"
-powershell -NoLogo -NoProfile -C "& { (New-Object System.Net.WebClient).DownloadFile('%ZIPURL%', '%ZIPFILE%') }"
+SET BENCH_ZIPURL=https://github.com/mastersign/bench/archive/bench-lib.zip
+SET BENCH_ZIPFILE=%ROOT%bench.zip
+SET BENCH_SUBFLDR=\bench-bench-lib
+SET BENCH_DIR=%ROOT%
+SET BENCHMGR_ZIPURL=http://192.168.0.17:8080/BenchManager.zip
+SET BENCHMGR_ZIPFILE=%ROOT%BenchManager.zip
+SET BENCHMGR_SUBFLDR=
+SET BENCHMGR_DIR=%ROOT%auto\bin
+
+PUSHD "%ROOT%"
+
+CALL :DOWNLOAD "%BENCH_ZIPURL%" "%BENCH_ZIPFILE%"
+CALL :DOWNLOAD "%BENCHMGR_ZIPURL%" "%BENCHMGR_ZIPFILE%"
+
+CALL :EXTRACT "%BENCH_ZIPFILE%" "%BENCH_SUBFLDR%" "%BENCH_DIR%"
+IF NOT EXIST "%BENCHMGR_DIR%\" MKDIR "%BENCHMGR_DIR%"
+CALL :EXTRACT "%BENCHMGR_ZIPFILE%" "%BENCHMGR_SUBFLDR%" "%BENCHMGR_DIR%"
+
+ECHO.Deleting ZIP archive(s) ...
+DEL "%BENCH_ZIPFILE%"
+DEL "%BENCHMGR_ZIPFILE%"
+
+ECHO.Running setup script ...
+.\actions\bench-ctl.cmd setup
+
+POPD
+GOTO:EOF
+
+:DOWNLOAD
+REM 1: URL, 2: target file
+ECHO.Downloading ZIP archive...
+ECHO.  %~1
+PUSHD "%ROOT%"
+powershell -NoLogo -NoProfile -C "& { (New-Object System.Net.WebClient).DownloadFile('%~1', '%~2') }"
 IF %ERRORLEVEL% NEQ 0 (
-  ECHO.Download failed.
+  ECHO.Download failed:
+  ECHO.  %~1
   PAUSE
   EXIT /B 1
 )
+POPD
+GOTO:EOF
 
+:EXTRACT
+REM 1: ZIP file, 2: sub-folder, 3: target dir
 ECHO.Extracting ZIP archive ...
-CD "%ROOT%"
-powershell -NoLogo -NoProfile -C "& { $ws = New-Object -ComObject Shell.Application; $zip = $ws.NameSpace('%ZIPFILE%\bench-master'); $trg = $ws.NameSpace('%ROOT%'); foreach($item in $zip.items()) { $trg.copyhere($item) } }"
+ECHO.  %~1
+PUSHD "%~3"
+powershell -NoLogo -NoProfile -C "& { $ws = New-Object -ComObject Shell.Application; $zip = $ws.NameSpace('%~1%~2'); $trg = $ws.NameSpace('%~3'); foreach($item in $zip.items()) { $trg.copyhere($item) } }"
 IF %ERRORLEVEL% NEQ 0 (
   ECHO.Extracting ZIP archive failed.
   PAUSE
   EXIT /B 1
 )
-
-ECHO.Deleting ZIP archive ...
-CD "%ROOT%"
-DEL "%ZIPFILE%"
-
-ECHO.Running setup script ...
-CD %ROOT%
-.\actions\bench-ctl.cmd setup
+POPD
