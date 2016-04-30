b3aa5cf78fbc632b6cee7fefbe5fea85f60d5490
Tobias Kiertscher <dev@mastersign.de>
2016-04-30 10:19:52 +0200

    fixed uninstall script for spacemacs

--- auto/apps/spacemacs.remove.ps1
+++ auto/apps/spacemacs.remove.ps1
@@ -1,7 +1,6 @@
 $homeDir = Get-ConfigValue HomeDir
 $spacemacsConfig = [IO.Path]::Combine($homeDir, ".spacemacs")
 $spacemacsConfigDir = [IO.Path]::Combine($homeDir, ".spacemacs.d")
-$spacemacsInitFile = [IO.Path]::Combine($spacemacsConfigDir, "init.el")
 $spacemacsDir = [IO.Path]::Combine($homeDir, ".emacs.d")
 
 if ((Test-Path $spacemacsConfig) -or (Test-Path $spacemacsConfigDir))
@@ -9,9 +8,5 @@ if ((Test-Path $spacemacsConfig) -or (Test-Path $spacemacsConfigDir))
     if (Test-Path $spacemacsDir)
     {
         Purge-Dir $spacemacsDir
-    }
-    if (Test-Path $spacemacsInitFile)
-    {
-        Remove-Item $spacemacsInitFile
     }
 }
