b5769e43d8d3c65fd6d5f111ca52af4a71f74fc0
Tobias Kiertscher <dev@mastersign.de>
2016-08-03 13:51:10 +0200

    added XML doc comments

--- BenchManager/BenchLib/AppArchiveTyps.cs
+++ BenchManager/BenchLib/AppArchiveTyps.cs
@@ -4,12 +4,24 @@ using System.Text;
 
 namespace Mastersign.Bench
 {
-    internal static class AppArchiveTyps
+    /// <summary>
+    /// This static class contains string constants for the supported archive typs.
+    /// </summary>
+    public static class AppArchiveTyps
     {
+        /// <summary>Automatic detection of the archive typ.</summary>
         public const string Auto = "auto";
+
+        /// <summary>A generic archive supported by 7-Zip.</summary>
         public const string Generic = "generic";
+
+        /// <summary>An MSI package, supported by LessMSI.</summary>
         public const string Msi = "msi";
+
+        /// <summary>An Inno Setup Program, supported by Inno Setup Unpacker.</summary>
         public const string InnoSetup = "inno";
+
+        /// <summary>A custom archive, which will be extracted by a custom extraction script.</summary>
         public const string Custom = "custom";
     }
 }
