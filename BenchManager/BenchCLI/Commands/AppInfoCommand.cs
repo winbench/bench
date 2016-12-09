402eb802776c8ca865093d6fc17b6d9c6d915ec1
Tobias Kiertscher <dev@mastersign.de>
2016-12-09 16:46:14 +0100

    implemented detailed app info

--- BenchManager/BenchCLI/Commands/AppInfoCommand.cs
+++ BenchManager/BenchCLI/Commands/AppInfoCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using Mastersign.CliTools;
 using Mastersign.Docs;
@@ -18,6 +19,8 @@ namespace Mastersign.Bench.Cli.Commands
         public override string Name => CMD_NAME;
 
         private DocumentOutputFormat Format = DEF_FORMAT;
+
+        private BenchConfiguration config;
 
         protected override ArgumentParser InitializeArgumentParser()
         {
@@ -55,14 +58,14 @@ namespace Mastersign.Bench.Cli.Commands
         {
             var appId = Arguments.GetPositionalValue(POSITIONAL_APP_ID);
 
-            var cfg = LoadConfiguration();
-            if (!cfg.Apps.Exists(appId))
+            config = LoadConfiguration();
+            if (!config.Apps.Exists(appId))
             {
                 WriteError("Unknown app ID: " + appId);
                 return false;
             }
 
-            var app = cfg.Apps[appId];
+            var app = config.Apps[appId];
             using (var w = DocumentWriterFactory.Create(Format, Console.OpenStandardOutput()))
             {
                 WriteAppInfo(app, w);
@@ -75,9 +78,75 @@ namespace Mastersign.Bench.Cli.Commands
         {
             writer.Begin(BlockType.Document);
             writer.Title(app.Label);
-
+            writer.Headline2("Description");
             writer.Begin(BlockType.List);
-            WriteProperty(writer, "ID", app.ID);
+            WriteProperty(writer, "ID", app.ID, InlineType.Keyword);
+            WriteProperty(writer, "Label", app.Label);
+            if (app.IsInstalled && app.Launcher != null)
+            {
+                WriteProperty(writer, "Launcher", app.Launcher);
+            }
+            WriteProperty(writer, "App Type", app.Typ, InlineType.Keyword);
+            WriteProperty(writer, "Version", app.Version, InlineType.Keyword);
+            writer.End(BlockType.List);
+            writer.Headline2("State");
+            writer.Paragraph(app.LongStatus);
+            var dependencies = app.Dependencies;
+            var responsibilities = app.Responsibilities;
+            if (dependencies.Length > 0 || responsibilities.Length > 0)
+            {
+                writer.Headline2("Relationships");
+                writer.Begin(BlockType.List);
+                if (dependencies.Length > 0)
+                {
+                    Array.Sort(dependencies);
+                    writer.Begin(BlockType.ListItem)
+                        .Text("Dependencies:")
+                        .Begin(BlockType.List);
+                    foreach (var d in dependencies)
+                    {
+                        writer.Begin(BlockType.ListItem)
+                            .Keyword(d)
+                            .End(BlockType.ListItem);
+                    }
+                    writer.End(BlockType.List);
+                    writer.End(BlockType.ListItem);
+                }
+                if (responsibilities.Length > 0)
+                {
+                    Array.Sort(responsibilities);
+                    writer.Begin(BlockType.ListItem)
+                        .Text("Responsibilities:")
+                        .Begin(BlockType.List);
+                    foreach (var r in app.Responsibilities)
+                    {
+                        writer.Begin(BlockType.ListItem)
+                            .Keyword(r)
+                            .End(BlockType.ListItem);
+                    }
+                    writer.End(BlockType.List);
+                    writer.End(BlockType.ListItem);
+                }
+                writer.End(BlockType.List);
+            }
+            writer.Headline2("Paths and Resources");
+            writer.Begin(BlockType.List);
+            if (app.IsInstalled)
+            {
+                WriteProperty(writer, "Installation Dir", app.Dir, InlineType.Keyword);
+                WriteProperty(writer, "Main Executable", app.Exe, InlineType.Keyword);
+            }
+            if (app.IsResourceCached)
+            {
+                WriteProperty(writer, "Cached Resource",
+                    Path.Combine(config.GetStringValue(PropertyKeys.DownloadDir),
+                        app.ResourceArchiveName ?? app.ResourceFileName),
+                    InlineType.Keyword);
+            }
+            if (app.Url != null)
+            {
+                WriteProperty(writer, "Resource URL", app.Url);
+            }
             writer.End(BlockType.List);
 
             writer.End(BlockType.Document);
@@ -87,8 +156,15 @@ namespace Mastersign.Bench.Cli.Commands
         {
             writer
                 .Begin(BlockType.ListItem)
-                .Keyword(key).Text(": ").Syntactic(value)
+                .Text(key).Text(": ").Text(value)
                 .End(BlockType.ListItem);
+        }
+        private void WriteProperty(DocumentWriter writer, string key, string value, InlineType type)
+        {
+            writer.Begin(BlockType.ListItem);
+            writer.Text(key).Text(": ");
+            writer.Inline(type, value);
+            writer.End(BlockType.ListItem);
         }
     }
 }
