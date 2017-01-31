42c8f9e7312efa37e74469f5897e6de4543581dd
Tobias Kiertscher <dev@mastersign.de>
2017-01-31 13:47:26 +0100

    added WinForms support to select the target directory

--- auto/lib/Initialize-ExportedBench.ps1
+++ auto/lib/Initialize-ExportedBench.ps1
@@ -1,9 +1,31 @@
 $myDir = [IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
 $sourceDir = Resolve-Path "$myDir\..\.."
 
-$targetDir = $(Read-Host "Zielverzeichnis")
+[void][System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+$browser = New-Object System.Windows.Forms.FolderBrowserDialog
+$browser.SelectedPath = $env:SystemDrive
+$browser.ShowNewFolderButton = $true
+$browser.Description = "Select the target directory for the cloned Bench environment."
+$result = $browser.ShowDialog()
+if ($result -ne "OK") { return }
 
+$targetDir = $browser.SelectedPath
+if (gci $targetDir)
+{
+	$answer = [System.Windows.Forms.MessageBox]::Show(
+		"The selected directory is not empty.`n`nAre you sure you want to setup Bench in this directory?",
+		"Bench Transfer Package",
+		"YesNo",
+		"Warning"
+	)
+	if ($answer -ne "Yes") { return }
+}
 if (!(Test-Path $targetDir)) { $_ = mkdir $targetDir }
-copy "$sourceDir\*" "$targetDir\" -Recurse -Force
+
+$folders = @("auto", "res", "config")
+foreach ($f in $folders)
+{
+	copy "$sourceDir\$f" "$targetDir\$f" -Recurse -Force
+}
 
 Start-Process -FilePath "$targetDir\auto\bin\bench.exe" -ArgumentList @("--verbose", "manage", "initialize")
