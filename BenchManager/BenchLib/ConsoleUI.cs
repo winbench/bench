bb23b8652f11ec04db8fa53183c89280086bedb0
Tobias Kiertscher <dev@mastersign.de>
2017-02-01 15:43:06 +0100

    refactored IUserInterface, removed obsolete dialogs

--- BenchManager/BenchLib/ConsoleUI.cs
+++ BenchManager/BenchLib/ConsoleUI.cs
@@ -12,14 +12,57 @@ namespace Mastersign.Bench
     /// </summary>
     public class ConsoleUserInterface : IUserInterface
     {
+        private static readonly object consoleSyncHandle = new object();
+
+        private void Show(string typ, string topic, string message,
+            ConsoleColor? color = null,
+            string detailedMessage = null,
+            Exception exception = null)
+        {
+            lock (consoleSyncHandle)
+            {
+                if (color.HasValue) Console.ForegroundColor = color.Value;
+
+                Console.WriteLine(
+                    "[{0}] {1}: {2}",
+                    typ,
+                    topic,
+                    message);
+
+                if (!string.IsNullOrEmpty(detailedMessage))
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine(detailedMessage);
+                }
+                if (exception != null)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(exception.ToString());
+                }
+                Console.ResetColor();
+            }
+        }
+
+        /// <summary>
+        /// Displays a detail message to the user.
+        /// </summary>
+        /// <param name="topic">The topic of the message.</param>
+        /// <param name="message">The message.</param>
+        /// <param name="detailedMessage">Additional information.</param>
+        public void ShowVerbose(string topic, string message, string detailedMessage = null)
+        {
+            Show("VERBOSE", topic, message, color: ConsoleColor.DarkCyan);
+        }
+
         /// <summary>
         /// Displays an informational message to the user.
         /// </summary>
         /// <param name="topic">The topic of the message.</param>
         /// <param name="message">The message.</param>
-        public void ShowInfo(string topic, string message)
+        /// <param name="detailedMessage">Additional information.</param>
+        public void ShowInfo(string topic, string message, string detailedMessage = null)
         {
-            Console.WriteLine("[INFO] " + topic + ": " + message);
+            Show("INFO", topic, message, detailedMessage: detailedMessage);
         }
 
         /// <summary>
@@ -27,9 +70,15 @@ namespace Mastersign.Bench
         /// </summary>
         /// <param name="topic">The topic of the message.</param>
         /// <param name="message">The message.</param>
-        public void ShowWarning(string topic, string message)
+        /// <param name="detailedMessage">Additional information.</param>
+        /// <param name="exception">An <see cref="Exception"/> object, causing the warning.</param>
+        public void ShowWarning(string topic, string message, string detailedMessage = null,
+            Exception exception = null)
         {
-            Console.WriteLine("[WARNING] " + topic + ": " + message);
+            Show("WARNING", topic, message,
+                color: ConsoleColor.Yellow,
+                detailedMessage: detailedMessage,
+                exception: exception);
         }
 
         /// <summary>
@@ -37,90 +86,15 @@ namespace Mastersign.Bench
         /// </summary>
         /// <param name="topic">The topic of the message.</param>
         /// <param name="message">The message.</param>
-        public void ShowError(string topic, string message)
+        /// <param name="detailedMessage">Additional information.</param>
+        /// <param name="exception">An <see cref="Exception"/> object, causing the warning.</param>
+        public void ShowError(string topic, string message, string detailedMessage = null,
+            Exception exception = null)
         {
-            Console.WriteLine("[ERROR] " + topic + ": " + message);
-        }
-
-        /// <summary>
-        /// Prompts the user to input the necessary information
-        /// about a Bench user.
-        /// </summary>
-        /// <param name="prompt">A string, shown to the user as prompt.</param>
-        /// <returns>A new instance of <see cref="BenchUserInfo"/> with the read input from the user.</returns>
-        public BenchUserInfo ReadUserInfo(string prompt)
-        {
-            var userInfo = new BenchUserInfo();
-            Console.WriteLine(prompt);
-            Console.Write("Username: ");
-            userInfo.Name = Console.ReadLine();
-            Console.Write("Email: ");
-            userInfo.Email = Console.ReadLine();
-            return userInfo;
-        }
-
-        /// <summary>
-        /// Reads a password from the user in a safe fashion.
-        /// </summary>
-        /// <param name="prompt">A string, shown to the user as prompt.</param>
-        /// <returns>An instance of <see cref="SecureString"/>.</returns>
-        public SecureString ReadPassword(string prompt)
-        {
-            Console.WriteLine(prompt);
-
-            // http://stackoverflow.com/questions/3404421/password-masking-console-application
-
-            Console.Write("Password: ");
-            SecureString pwd = new SecureString();
-            while (true)
-            {
-                ConsoleKeyInfo i = Console.ReadKey(true);
-                if (i.Key == ConsoleKey.Enter)
-                {
-                    break;
-                }
-                else if (i.Key == ConsoleKey.Backspace)
-                {
-                    if (pwd.Length > 0)
-                    {
-                        pwd.RemoveAt(pwd.Length - 1);
-                        //Console.Write("\b \b");
-                    }
-                }
-                else
-                {
-                    pwd.AppendChar(i.KeyChar);
-                    //Console.Write("*");
-                }
-            }
-            Console.WriteLine();
-            return pwd;
-        }
-
-        /// <summary>
-        /// Allow the user to edit the given text file.
-        /// This methods returns, when the user ends the editing.
-        /// </summary>
-        /// <param name="path">A path to the text file to edit.</param>
-        public void EditTextFile(string path)
-        {
-            var p = Process.Start(
-            Path.Combine(Environment.GetEnvironmentVariable("SystemRoot"), "notepad.exe"),
-                path);
-            p.WaitForExit();
-        }
-
-        /// <summary>
-        /// Allow the user to edit the given text file.
-        /// This methods returns, when the user ends the editing.
-        /// </summary>
-        /// <param name="path">A path to the text file to edit.</param>
-        /// <param name="prompt">A string, shown to the user as prompt.</param>
-        public void EditTextFile(string path, string prompt)
-        {
-            Console.WriteLine(prompt);
-            Console.WriteLine("Close the text editor to proceed ...");
-            EditTextFile(path);
+            Show("ERROR", topic, message,
+                color: ConsoleColor.Red,
+                detailedMessage: detailedMessage,
+                exception: exception);
         }
     }
 }
