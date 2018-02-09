36d2c446b78c260fd9e9bcd47534907d38cef5e7
Tobias Kiertscher <dev@mastersign.de>
2018-02-09 14:19:49 +0100

    improved landing page and download buttons

--- docs/src-static/js/download.js
+++ docs/src-static/js/download.js
@@ -1,32 +1,25 @@
 function GetLatestReleaseInfo() {
   $.getJSON("https://api.github.com/repos/mastersign/bench/releases/latest")
     .done(function(release) {
-        var asset = null;
+        var setupAsset = null;
+        var archiveAsset = null;
         for (var i = 0; i < release.assets.length; i++) {
             if (release.assets[i].name == "BenchSetup.exe") {
-                asset = release.assets[i];
-                break;
+                setupAsset = release.assets[i];
+            }
+            if (release.assets[i].name == "Bench.zip") {
+                archiveAsset = release.assets[i];
             }
         }
-        if (!asset) return;
-        // var downloadCount = 0;
-        // for (var i = 0; i < release.assets.length; i++) {
-        //   downloadCount += release.assets[i].download_count;
-        // }
-        // var oneHour = 60 * 60 * 1000;
-        // var oneDay = 24 * oneHour;
-        // var dateDiff = new Date() - new Date(asset.updated_at);
-        // var timeAgo;
-        // if (dateDiff < oneDay) {
-        //   timeAgo = (dateDiff / oneHour).toFixed(1) + " hours ago";
-        // } else {
-        //   timeAgo = (dateDiff / oneDay).toFixed(1) + " days ago";
-        // }
-        // var releaseInfo = release.name + " was updated " + timeAgo + " and downloaded " + downloadCount.toLocaleString() + " times.";
-        // console.log(releaseInfo);
         var name = release.name.replace(/^v/, '');
-        $(".setup-download-button").attr("href", asset.browser_download_url);
-        $(".setup-download-link").attr("href", asset.browser_download_url);
         $(".version-info").text(name);
+        if (setupAsset) {
+            $(".setup-download-button").attr("href", setupAsset.browser_download_url);
+            $(".setup-download-link").attr("href", setupAsset.browser_download_url);
+        }
+        if (archiveAsset) {
+            $(".archive-download-button").attr("href", archiveAsset.browser_download_url);
+            $(".archive-download-link").attr("href", archiveAsset.browser_download_url);
+        }
       });
 }
