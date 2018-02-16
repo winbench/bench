59ef8050e022b8b2926481270a41fd4bc2bf0895
Tobias Kiertscher <dev@mastersign.de>
2018-02-16 10:52:23 +0100

    fixed entrypoint for Git Bash bench-bash.cmd

--- BenchManager/scripts/bench-bash.cmd
+++ BenchManager/scripts/bench-bash.cmd
@@ -5,8 +5,8 @@ IF "_%1_" == "__" (
    ECHO.BENCH v%BENCH_VERSION% BASH
 )
 
-IF EXIST "%BENCH_APPS%\git\bin\bash.exe" (
-  CALL "%BENCH_APPS%\git\bin\bash.exe" %*
+IF EXIST "%BENCH_APPS%\bench\git\bin\bash.exe" (
+  CALL "%BENCH_APPS%\bench\git\bin\bash.exe" %*
 ) ELSE (
   ECHO.No Bash executable found. Is Git installed?
 )
